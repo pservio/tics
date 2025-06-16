2eb0db58bb5f41070f0ceb2e476f6a625a89b6a3
pservio <pservio@gmail.com>
2025-06-16 18:13:01 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,1 @@
-// ATENÇÃO: NUNCA envie este ficheiro para o GitHub!
-// Adicione a linha "firebase-config.js" ao seu ficheiro .gitignore
 
-// Substitua os valores abaixo pelos seus próprios dados do Firebase
-// Pode encontrar estes dados no seu projeto Firebase:
-// Definições do projeto > Geral > As suas apps > App da Web > Configuração
-window.firebaseConfig = {
-  apiKey: "AIzaSyBST9y9zpxP_gnbmx8swYfb_k5nyWfgWxo",
-  authDomain: "simulado-enade-artes-visuais.firebaseapp.com",
-  projectId: "simulado-enade-artes-visuais",
-  storageBucket: "simulado-enade-artes-visuais.firebasestorage.app",
-  messagingSenderId: "506173130361",
-  appId: "1:506173130361:web:c2126a5603b6f63d9ff6a8",
-  measurementId: "G-WYXVBQ12MK"
-};
-
-// Substitua pelo ID que deseja para sua aplicação no ranking.
-// Este nome pode ser o que quiser.
-window.appId = "quiz-enade-artes-v1";
